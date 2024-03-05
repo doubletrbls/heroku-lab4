2d3c161b7ec4ee3b3764747043082de30ef76cd3
sarvarsaidazov <mrsarvar418@gmail.com>
2024-03-06 00:11:42 +0500

    added maven project

---
+++ src/main/java/com/mavenproject2/JakartaRestConfiguration.java
@@ -0,0 +1,11 @@
+package com.mavenproject2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+
+
+}
